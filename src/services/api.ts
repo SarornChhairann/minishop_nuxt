2a545b9389797693458fc932727a04549b1f7c1a
Chhaiann <chhaiann@Chhaianns-MacBook-Pro.local>
2025-12-22 11:53:34 +0700

    fix base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://minishop-api.vercel.app/api',
+    baseURL: 'https://vanny-minishop-fr2h.vercel.app/api',
     headers: {
         'Cache-Control': 'no-cache'
     }
